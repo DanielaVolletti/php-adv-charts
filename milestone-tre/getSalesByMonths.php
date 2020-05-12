6cdbfd1c987e8b54edafbd57bbdc9d284352410a
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-05-12 16:34:52 +0200

    milestone 3 grafico

---
+++ milestone-tre/getSalesByMonths.php
@@ -0,0 +1,9 @@
+<?php
+  header('Content-Type: application/json');
+
+  require_once 'database.php';
+
+  $grafLine = $graphs['fatturato'];
+
+  echo json_encode($grafLine);
+ ?>
---
+++ milestone-tre/getThreeGrafic.php
@@ -0,0 +1,22 @@
+<?php
+  header('Content-Type: application/json');
+
+  require_once 'database.php';
+
+  $threeGraf = $graphs['team_efficiency'];
+  $team = [];
+  $trend = [];
+  foreach ($threeGraf['data'] as $key => $value) {
+    $team[] = $key;
+    $trend[] = $value;
+  };
+
+  $res = [
+    'type' => $threeGraf['type'],
+    'team' => $team,
+    'trend' => $trend
+  ];
+
+  echo json_encode($res);
+
+?>
